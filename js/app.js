dd6ca78a912503cb239f500896c7147a9715e119
Family <family@Familys-iMac.home>
2016-05-25 19:11:24 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,134 @@
+$(function() {
+    $( "#draggable" ).draggable();
+  });
+
+	var zoomCount = 1;
+  $( document ).ready(function() {
+
+  	$(function() {
+    $( "#slider1,#slider2" ).slider({
+    	min: 1,
+    	max: 9
+	});
+
+    $("#hmap").click( function() {
+	if($("#hmap").prop("checked") == false) {
+		
+		$( ".maporg" ).removeClass( "visible" );
+		$( ".maporg" ).addClass( "hidden" );
+	} else {
+		$( ".maporg" ).removeClass( "hidden" );
+		$( ".maporg" ).addClass( "visible" );
+	}
+    });
+
+	$("#dmap").click( function() {
+	if($("#dmap").prop("checked") == false) {	
+		$( ".map3d" ).removeClass( "visible" );
+		$( ".map3d" ).addClass( "hidden" );
+	} else {
+		$( ".map3d" ).removeClass( "hidden" );
+		$( ".map3d" ).addClass( "visible" );
+	}
+    });
+
+
+  });
+
+
+    $(".zoom-plus").click( function() {
+    
+	  	
+	  	 if (zoomCount === 1) {
+	  	 	$("#draggable").css("transform","Scale(1)");
+	  	 	zoomCount++;
+	  	 } else if(zoomCount === 2){
+	  	 	$("#draggable").css("transform","Scale(2)");
+	  	 	zoomCount++;
+	  	 } else if(zoomCount === 3) {
+	  	 	$("#draggable").css("transform","Scale(3)");
+	  	 	zoomCount++;
+	  	 } else {
+	  	 	$("#draggable").css("transform","Scale(4)");
+	  	 }
+
+  });
+
+    $(".zoom-minus").click( function() {
+    
+	  	
+	  	 if (zoomCount === 1) {
+	  	 	$("#draggable").css("transform","Scale(1)");
+	  	 } else if(zoomCount === 2){
+	  	 	$("#draggable").css("transform","Scale(1)");
+	  	 	zoomCount--;
+	  	 } else if(zoomCount === 3) {
+	  	 	$("#draggable").css("transform","Scale(2)");
+	  	 	zoomCount--;
+	  	 } else {
+	  	 	$("#draggable").css("transform","Scale(3)");
+	  	 	zoomCount--;
+	  	 }
+
+  });
+
+	
+  
+
+$("#slider1").click(function(){
+	var value = $("#slider1").slider("value");
+	
+
+	$(".map3d").css("opacity","0.4");
+
+	switch(value) {
+		case 1: $(".map3d").css("opacity", "1");
+		break;
+		case 2: $(".map3d").css("opacity", "0.9");
+		break;
+		case 3: $(".map3d").css("opacity", "0.8");
+		break;
+		case 4: $(".map3d").css("opacity", "0.7");
+		break;
+		case 5: $(".map3d").css("opacity", "0.6");
+		break;
+		case 6: $(".map3d").css("opacity", "0.5");
+		break;
+		case 7: $(".map3d").css("opacity", "0.4");
+		break;
+		case 8: $(".map3d").css("opacity", "0.3");
+		break;
+		case 9: $(".map3d").css("opacity", "0.2");
+		break;
+	}
+
+});
+
+$("#slider2").click(function(){
+	var value = $("#slider2").slider("value");
+	
+
+	switch(value) {
+		case 1: $(".maporg").css("opacity", "1");
+		break;
+		case 2: $(".maporg").css("opacity", "0.9");
+		break;
+		case 3: $(".maporg").css("opacity", "0.8");
+		break;
+		case 4: $(".maporg").css("opacity", "0.7");
+		break;
+		case 5: $(".maporg").css("opacity", "0.6");
+		break;
+		case 6: $(".maporg").css("opacity", "0.5");
+		break;
+		case 7: $(".maporg").css("opacity", "0.4");
+		break;
+		case 8: $(".maporg").css("opacity", "0.3");
+		break;
+		case 9: $(".maporg").css("opacity", "0.2");
+		break;
+	}
+
+});
+
+  });
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Created By: Carlos Merced
+Date: May 25, 2016
+Title: Brooklyn in the year of 1827
+
+** Works in Firefox and Google Chrome updated browsers **
+
+This 3d model of Brooklyn shows the land occupied or sold in 1827. The blue represents occupied space and the tan color represents undeveloped or unsold land plots.
+
+Click on index.html to view the interactive map.
